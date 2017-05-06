7b77ef7986b4e149eb6bed0f600c3e6c768016d7
p-bhargavi <paturibhargavi.123@gmail.com>
2017-05-06 00:26:12 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'webpages/new'
+
+  get 'webpages/edit'
+
+  get 'webpages/index'
+
+  get 'webpages/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ test/controllers/webpages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class WebpagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get webpages_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get webpages_edit_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get webpages_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get webpages_show_url
+    assert_response :success
+  end
+
+end
